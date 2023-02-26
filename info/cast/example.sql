a0dca8008176777fb5d702fbb046305791420b5d
yarmail <yarmail@yandex.ru>
2023-02-26 14:40:51 +0300

    editing

---
+++ info/cast/example.sql
@@ -0,0 +1,35 @@
+/*
+Тема: Приведение типов
+Практический пример приведения типов
+Создадим функцию, которая ничего не возвращает
+    RAISE NOTICE 'ran %', money_val;
+логируем переменную money_val
+ */
+CREATE OR REPLACE FUNCTION type_testing(money_val int)
+RETURNS void AS $$
+    BEGIN
+        RAISE NOTICE 'run %', money_val;
+    END;
+$$ LANGUAGE plpgsql;
+
+SELECT type_testing(1);
+-- Output: run 1
+
+-- SELECT type_testing(0.5);
+-- Output: ошибка
+
+SELECT type_testing(0.5::int);
+SELECT type_testing(CAST(0.5 AS int));
+-- Output: run 1
+-- приведение к целому округляется до ближайшего целого
+-- (два одинаковых примера) один короткий, другой через CAST
+
+SELECT type_testing('1.5'::numeric::int);
+-- Output: 2
+-- Можно преобразовать Строку в число через numeric
+
+SELECT 'abc' || 1;
+-- Output: abc1
+
+SELECT ' 10 ' = 10;
+-- Output: true
